22030c7041e415aef112dd6977cb7b59e2020887
itedya <bdomanski@protonmail.com>
2023-12-29 16:02:46 +0100

    add basichashmap_s's 'vector' property initialization into init func

--- basichashmap.c
+++ basichashmap.c
@@ -7,9 +7,14 @@ struct basichashmap_s {
 };
 
 int basichashmap_init(struct basichashmap_s **hashmap) {
-    *hashmap = malloc(sizeof(struct basichashmap_s));
+    struct basichashmap_s *new_hashmap = malloc(sizeof(struct basichashmap_s));
     
-    if (*hashmap == NULL) {
+    if (new_hashmap == NULL) {
+        return BASICHASHMAP_MEMORY_ERROR;
+    }
+
+    if (basicvector_init(&new_hashmap->vector) != BASICVECTOR_SUCCESS) {
+        free(new_hashmap);
         return BASICHASHMAP_MEMORY_ERROR;
     }
 
