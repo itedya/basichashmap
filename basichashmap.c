67aa28cf11be429314e102daf999fb1bcd713c8b
itedya <bdomanski@protonmail.com>
2023-12-29 18:10:01 +0100

    Add basichashmap_free function

--- basichashmap.c
+++ basichashmap.c
@@ -6,6 +6,11 @@ struct basichashmap_s {
     struct basicvector_s *vector;
 };
 
+struct basichashmap_entry_s {
+    char *index;
+    void *data;
+};
+
 int basichashmap_init(struct basichashmap_s **hashmap) {
     struct basichashmap_s *new_hashmap = malloc(sizeof(struct basichashmap_s));
     
@@ -17,6 +22,42 @@ int basichashmap_init(struct basichashmap_s **hashmap) {
         free(new_hashmap);
         return BASICHASHMAP_MEMORY_ERROR;
     }
+
+    return BASICHASHMAP_SUCCESS;
+}
+
+void basichashmap_entry_s_deallocation_function(void *entry) {
+    struct basichashmap_entry_s *hashmap_entry = (struct basichashmap_entry_s *) entry;
+
+    free(hashmap_entry->index);
+
+    free(hashmap_entry);
+}
+
+int basichashmap_free(struct basichashmap_s *hashmap, void (* deallocation_function)(void *item)) {
+    if (hashmap == NULL) {
+        return BASICHASHMAP_MEMORY_ERROR;
+    }
+
+    int length;
+
+    if (basicvector_length(hashmap->vector, &length) != BASICVECTOR_SUCCESS) {
+        return BASICHASHMAP_MEMORY_ERROR;
+    }
+
+    if (deallocation_function != NULL) {
+        for (int i = 0; i < length; i++) {
+            struct basichashmap_entry_s *entry;
+
+            if (basicvector_get(hashmap->vector, i, (void **) &entry) != BASICVECTOR_SUCCESS) {
+                return BASICHASHMAP_MEMORY_ERROR;
+            }
+
+            deallocation_function(entry->data);
+        }
+    }
+
+    basicvector_free(hashmap->vector, basichashmap_entry_s_deallocation_function);
 
     return BASICHASHMAP_SUCCESS;
 }
--- basichashmap.h
+++ basichashmap.h
@@ -13,4 +13,17 @@ struct basichashmap_s;
 //  BASICHASHMAP_SUCCESS        - when everything is ok
 int basichashmap_init(struct basichashmap_s **hashmap);
 
+/*
+ * Frees the hashmap
+ *
+ * Params:
+ *  hashmap                 - Pointer to hashmap struct.
+ *  deallocation_function   - Function that will be run against every existing hashmap item for deallocation purposes. If provided null (for ex. because elements are on stack), function execution will be skipped.
+ *
+ * Returns:
+ *  BASICHASHMAP_SUCCESS        - when everything went ok
+ *  BASICHASHMAP_MEMORY ERROR   - when provided hashmap is NULL or if error inside the function occured
+*/
+int basichashmap_free(struct basichashmap_s *hashmap, void (* deallocation_function)(void *item));
+
 #endif // BASICHASHMAP
