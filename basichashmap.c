c0df88aa5d2d824b7c3acc98c7e98877496669f5
itedya <bdomanski@protonmail.com>
2023-12-30 12:15:54 +0100

    add index and user_data parameters to dealloc callback in free func

--- basichashmap.c
+++ basichashmap.c
@@ -28,38 +28,32 @@ int basichashmap_init(struct basichashmap_s **hashmap) {
     return BASICHASHMAP_SUCCESS;
 }
 
-void basichashmap_entry_s_deallocation_function(void *entry) {
-    struct basichashmap_entry_s *hashmap_entry = (struct basichashmap_entry_s *) entry;
+// see usage of this struct in basichashmap_free for explanation
+struct deallocation_user_data_basicvector_free_s {
+    void (*deallocation_function)(void *index, void *item, void *user_data);
+    void *user_data;
+};
+
+void basichashmap_entry_s_deallocation_function(void *vector_item, void *user_data) {
+    struct deallocation_user_data_basicvector_free_s *deallocation_user_data = (struct deallocation_user_data_basicvector_free_s *) user_data;
+    struct basichashmap_entry_s *hashmap_entry = (struct basichashmap_entry_s *) vector_item;
+
+    deallocation_user_data->deallocation_function(hashmap_entry->index, hashmap_entry->data, user_data);
 
     free(hashmap_entry->index);
 
     free(hashmap_entry);
 }
 
-int basichashmap_free(struct basichashmap_s *hashmap, void (* deallocation_function)(void *item)) {
+int basichashmap_free(struct basichashmap_s *hashmap, void (* deallocation_function)(void *index, void *item, void *user_data), void *user_data) {
     if (hashmap == NULL) {
         return BASICHASHMAP_MEMORY_ERROR;
     }
 
-    int length;
+    // Type of deallocation function in basicvector library does not correspond to one that we have. Because of that, there is a struct that is only for passing data to compatible deallocation function and then from that data we can execute proper callback (see basichashmap_entry_s_deallocation_function).
+    struct deallocation_user_data_basicvector_free_s deallocation_user_data = { deallocation_function, user_data };
 
-    if (basicvector_length(hashmap->vector, &length) != BASICVECTOR_SUCCESS) {
-        return BASICHASHMAP_MEMORY_ERROR;
-    }
-
-    if (deallocation_function != NULL) {
-        for (int i = 0; i < length; i++) {
-            struct basichashmap_entry_s *entry;
-
-            if (basicvector_get(hashmap->vector, i, (void **) &entry) != BASICVECTOR_SUCCESS) {
-                return BASICHASHMAP_MEMORY_ERROR;
-            }
-
-            deallocation_function(entry->data);
-        }
-    }
-
-    basicvector_free(hashmap->vector, basichashmap_entry_s_deallocation_function);
+    basicvector_free(hashmap->vector, basichashmap_entry_s_deallocation_function, &deallocation_user_data);
 
     return BASICHASHMAP_SUCCESS;
 }
--- basichashmap.h
+++ basichashmap.h
@@ -14,16 +14,17 @@ struct basichashmap_s;
 int basichashmap_init(struct basichashmap_s **hashmap);
 
 /*
- * Frees the hashmap
+ * Frees the hashmap from memory
  *
  * Params:
  *  hashmap                 - Pointer to hashmap struct.
  *  deallocation_function   - Function that will be run against every existing hashmap item for deallocation purposes. If provided null (for ex. because elements are on stack), function execution will be skipped.
+ *  user_data               - Data that will be passed to deallocation function. If you need any context data in deallocation function, this is your saviour.
  *
  * Returns:
  *  BASICHASHMAP_SUCCESS        - when everything went ok
  *  BASICHASHMAP_MEMORY ERROR   - when provided hashmap is NULL or if error inside the function occured
 */
-int basichashmap_free(struct basichashmap_s *hashmap, void (* deallocation_function)(void *item));
+int basichashmap_free(struct basichashmap_s *hashmap, void (* deallocation_function)(void *index, void *item, void *user_data), void *user_data);
 
 #endif // BASICHASHMAP
